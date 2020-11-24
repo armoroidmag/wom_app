1c62229fc8a35f43c7af144e35c3c6b5edbbe44f
armoroid.mag <armoroidmag@gmail.com>
2020-11-24 11:49:44 +0900

    devise修正
    日本語化にしたため
     Please enter the commit message for your changes. Lines starting

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,14 @@ module Wordir
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
-
+    # 日本時間に設定
+    config.time_zone = "Tokyo"
+    config.active_record.default_timezone = :local
+    # カレンダー
+    config.beginning_of_week = :sunday
+    # 通知機能の「○分前」を日本語表示にするため
+    config.i18n.default_locale = :ja
+    # config/locales以下のディレクトリ内にある全てのymlファイルを読み込む（エラーメッセージの日本語化で使用）
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
