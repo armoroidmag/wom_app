1b841842847e7476e5f9852b826dd1bebccd97e8
armoroid.mag <armoroidmag@gmail.com>
2020-11-26 00:38:19 +0900

    [fix]新規登録の修正
    登録ができなくなったため

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  #mount_uploader :profile_image, ImageUploader
+  attachment :profile_image
 end
