8a3b9fa0d2d64959855c885a974931e3da12368d
armoroid.mag <armoroidmag@gmail.com>
2020-11-27 01:52:30 +0900

    refileで画像投稿実装
    ユーザー編集画面追記

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,10 +4,26 @@ class UsersController < ApplicationController
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   def edit
+    @user = User.find(params[:id])
+    
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to user_path(@user), notice: "ユーザー情報を更新しました。"
+    else
+      render :edit
+    end  
   end
 
 
+  private
+  def user_params
+    params.require(:user).permit(:username, :email, :profile, :profile_image)
+  end
 end
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '185c95c9749dde99eff2da29980801db662fc6faba64bc0ab31392f9ac40db509b4c5f74acd668f209422dbbcffb16675deed5dfc64d3e354de16768dd11fccf'
+
