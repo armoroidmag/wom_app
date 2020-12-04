d340dfa8b119b4998b0a1397b8d81ed6cfad8fec
armoroid.mag <armoroidmag@gmail.com>
2020-12-04 16:03:11 +0900

    import font-awsome

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,9 +11,9 @@ class UsersController < ApplicationController
 
   def edit
     @user = User.find(params[:id])
-    @user != current_user
+    if @user != current_user
     redirect_to users_path, alert: '不正なアクセスです'
-    
+    end
   end
 
   def update
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,9 @@ Rails.application.routes.draw do
   root to: "home#index"
   resources :users
   resources :items
+
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
+  
 end
